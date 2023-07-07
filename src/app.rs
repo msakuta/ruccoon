ea454fd0dace652797f4e61ea0d8d77d1b732975
msakuta <masahiro.sakuta@gmail.com>
2023-07-08 02:39:21 +0900

    Add randomly scattered corns

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,12 @@
-use std::rc::Rc;
+use std::{error::Error, rc::Rc};
 
 use eframe::{
     egui::{self, Frame, Painter, Response},
-    epaint::{ColorImage, Pos2, Rect},
+    epaint::{pos2, Color32, ColorImage, Pos2, Rect},
 };
 use image::{io::Reader as ImageReader, ImageError};
 
+use rand::Rng;
 use ruscal::{parse_args, Args};
 
 use crate::{
@@ -19,8 +20,10 @@ pub(crate) const BOARD_SIZE: usize = 12;
 
 pub(crate) struct RusFarmApp {
     bg: BgImage,
-    rascal_img: Option<eframe::egui::TextureHandle>,
+    rascal_img: Option<egui::TextureHandle>,
     rascals: Vec<Rascal>,
+    corn_img: Option<egui::TextureHandle>,
+    items: Vec<Pos2>,
     last_animate: Option<std::time::Instant>,
 }
 
@@ -41,6 +44,8 @@ impl RusFarmApp {
             bg: BgImage::new(),
             rascal_img: None,
             rascals: (0..2).map(|i| Rascal::new(i, &program)).collect(),
+            corn_img: None,
+            items: vec![],
             last_animate: None,
         }
     }
@@ -72,33 +77,30 @@ impl RusFarmApp {
                 )?;
             }
         }
-        if self.rascal_img.is_none() {
-            let file_name = "assets/rascal.png";
-            self.rascal_img = Some({
-                let img = ImageReader::open(file_name)?.decode()?.into_rgba8();
-                let width = img.width();
-                let height = img.height();
-                let data: Vec<_> = img.to_vec();
-                let color_image = egui::ColorImage::from_rgba_unmultiplied(
-                    [width as usize, height as usize],
-                    &data,
-                );
-                painter.ctx().load_texture(
-                    "rascal",
-                    color_image,
-                    egui::TextureOptions {
-                        magnification: egui::TextureFilter::Nearest,
-                        minification: egui::TextureFilter::Linear,
-                    },
-                )
-            });
+
+        fn try_insert_with<'a>(
+            target: &'a mut Option<egui::TextureHandle>,
+            file_name: &str,
+            painter: &Painter,
+        ) -> Option<&'a egui::TextureHandle> {
+            if target.is_none() {
+                *target = match try_load_image(file_name, painter) {
+                    Ok(res) => Some(res),
+                    Err(e) => {
+                        eprintln!("try_load_image({}) failed: {e}", file_name);
+                        None
+                    }
+                };
+            }
+            target.as_ref()
         }
 
-        if let Some(texture) = self.rascal_img.as_ref() {
-            let to_screen = egui::emath::RectTransform::from_to(
-                Rect::from_min_size(Pos2::ZERO, response.rect.size()),
-                response.rect,
-            );
+        let to_screen = egui::emath::RectTransform::from_to(
+            Rect::from_min_size(Pos2::ZERO, response.rect.size()),
+            response.rect,
+        );
+
+        if let Some(texture) = try_insert_with(&mut self.rascal_img, "assets/rascal.png", painter) {
             let size = texture.size_vec2();
             for rascal in &self.rascals {
                 let state = rascal.state.borrow();
@@ -112,12 +114,43 @@ impl RusFarmApp {
                 painter.image(texture.id(), to_screen.transform_rect(rect), UV, state.tint);
             }
         }
+
+        if let Some(texture) = try_insert_with(&mut self.corn_img, "assets/corn.png", painter) {
+            let size = texture.size_vec2();
+            for item in &self.items {
+                let min = item.to_vec2() * CELL_SIZE_F;
+                let max = min + size;
+                let rect = Rect {
+                    min: min.to_pos2(),
+                    max: max.to_pos2(),
+                };
+                const UV: Rect = Rect::from_min_max(Pos2::ZERO, Pos2::new(1.0, 1.0));
+                painter.image(
+                    texture.id(),
+                    to_screen.transform_rect(rect),
+                    UV,
+                    Color32::WHITE,
+                );
+            }
+        }
+
         Ok(())
     }
 
     fn animate(&mut self) {
         for rascal in &mut self.rascals {
             rascal.animate();
+        }
+
+        let mut rng = rand::thread_rng();
+        if self.items.len() < 10 && rng.gen::<f64>() < 0.1 {
+            let pos = pos2(
+                rng.gen_range(0..BOARD_SIZE) as f32,
+                rng.gen_range(0..BOARD_SIZE) as f32,
+            );
+            if self.items.iter().all(|item| *item != pos) {
+                self.items.push(pos);
+            }
         }
     }
 }
@@ -144,4 +177,24 @@ impl eframe::App for RusFarmApp {
             });
         });
     }
+}
+
+fn try_load_image(
+    file_name: &str,
+    painter: &Painter,
+) -> Result<egui::TextureHandle, Box<dyn Error>> {
+    let img = ImageReader::open(file_name)?.decode()?.into_rgba8();
+    let width = img.width();
+    let height = img.height();
+    let data: Vec<_> = img.to_vec();
+    let color_image =
+        egui::ColorImage::from_rgba_unmultiplied([width as usize, height as usize], &data);
+    Ok(painter.ctx().load_texture(
+        "rascal",
+        color_image,
+        egui::TextureOptions {
+            magnification: egui::TextureFilter::Nearest,
+            minification: egui::TextureFilter::Linear,
+        },
+    ))
 }
